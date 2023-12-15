6bc6c32d6dd0629f5e7cbad5d24e4dcd344bc928
unknown <yonideserbaix@gmail.com>
2023-12-15 19:52:44 +0100

    new commit

--- JS/script.js
+++ JS/script.js
@@ -71,7 +71,7 @@ $(document).ready(function () {
     animateImages(imagesAnimations[index]); // Animating the image based on the clicked index
   });
 
-  // Function to animate the navba
+  // Function to animate the navbar
   function animateNavbar(animation) {
     $($navbar).animate({ top: animation }, "slow");
   }
